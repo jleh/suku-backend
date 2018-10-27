ca470d00fd30a853d8ba9855d45b52973a073e95
Juuso Lehtinen <juuso.lehtinen@karttalehtinen.fi>
2018-10-27 15:34:22 +0300

    initial commit

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject suku-backend "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ragtime "0.7.2"]]
+  :plugins [[lein-ring "0.12.4"]]
+  :ring {:handler suku-backend.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
