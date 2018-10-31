fda6ff0bf1faabe632f8966452a2443c173ca341
Juuso Lehtinen <jupelehtinen@gmail.com>
2018-10-31 17:46:19 +0200

    Possible to get some person data from gedcom file

--- src/suku_backend/gedcom.clj
+++ src/suku_backend/gedcom.clj
@@ -12,3 +12,30 @@
 
 (defn get-individuals [data]
   (map #(get data %) (get-individual-keys data)))
+
+(defn get-first [data key]
+  (first (get data key)))
+
+(defn get-name [person]
+  (let [name-data (get-first person "NAME")]
+    {:first (:data (get-first name-data "GIVN"))
+     :last (:data (get-first name-data "SURN"))}))
+
+(defn get-date-and-place [event]
+  {:date (:data (get-first event "DATE"))
+   :place (:data (get-first event "PLAC"))})
+
+(defn get-birth [individual]
+  (get-date-and-place (get-first individual "BIRT")))
+
+(defn get-death [individual]
+  (get-date-and-place (get-first individual "DEAT")))
+
+(defn to-person [individual]
+  {:name (get-name individual)
+   :sex (:data (get-first individual "SEX"))
+   :birth (get-birth individual)
+   :death (get-death individual)})
+
+(defn list-persons [individuals]
+  (map #(to-person %) individuals))
--- src/suku_backend/places.clj
+++ src/suku_backend/places.clj
@@ -2,5 +2,12 @@
   (:require [clojure.java.jdbc :as jdbc]
             [suku-backend.db :as db]))
 
+(defn child? [place other]
+  (= (:id place) (:parent other)))
+
+(defn get-child-places [place places]
+  (map #(:id %) (filter #(child? place %) places)))
+
 (defn get-places []
-  (jdbc/query db/db ["select * from place"]))
+  (let [places (jdbc/query db/db ["select * from place"])]
+    (map #(assoc % :children (get-child-places % places)) places)))
